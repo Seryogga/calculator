fd1f7582e519a5714bee7609e5f9fc3302ef31e5
Seryogga <70600580+Seryogga@users.noreply.github.com>
2022-06-03 19:35:52 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <Calculator />
+  </div>
+</template>
+
+<script>
+import Calculator from "./components/BaseCalc.vue";
+
+export default {
+  name: "app",
+  components: {
+    Calculator,
+  },
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
+
+#app {
+  font-family: "Roboto";
+  max-width: 600px;
+  margin: 0 auto;
+  padding: 2rem;
+  font-weight: normal;
+}
+</style>
